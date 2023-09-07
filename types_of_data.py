a752b71b3fa2d4773fee749a1ffb71a96317d25c
EduardoDMarotta <eduardodouradomarotta@gmail.com>
2023-09-07 17:34:52 -0300

    modification in the files of DataTypes

--- types_of_data.py
+++ types_of_data.py
@@ -1,7 +1,7 @@
-print(1 + 4)     #Tipo int são numeros inteiros (Ex: 1, 2, -2, -6, 10000).
-print(1.5 * 2)   #Tipo float são numeros com ponto flutuante (Ex: 0.3, 2.5,10000.54).
-print("Steing")  #Tipo String são letras (Ex: a, A, b, B, c ).
-print(True)      #Tipo boolean é verdadeiro ou falso.
+print(1 + 4)         #Tipo int são numeros inteiros (Ex: 1, 2, -2, -6, 10000).
+print(1.5 * 2)       #Tipo float são numeros com ponto flutuante (Ex: 0.3, 2.5,10000.54).
+print("EduMarotta")  #Tipo String são letras (Ex: a, A, b, B, c ).
+print(True)          #Tipo boolean é verdadeiro ou falso.
 
 int()
 float()
